211c198c6f0babe4c678897ac0d683f2322a1e71
nikeedev <nikita.gon.dev@gmail.com>
2022-12-29 17:56:53 +0100

    Updates

--- learning_v.v
+++ learning_v.v
@@ -1,5 +1,22 @@
 module main
 
+struct Point {
+	x int
+	y int
+}
+
+fn (value []Point) print_all() {
+	for num in value {
+		println("$num.x, $num.y")
+	}
+}
+
 fn main() {
-	println('Hello World!')
+	mut boxes := []Point{}
+
+	boxes << Point{6, 6}
+
+	boxes << Point{65, 65}
+	boxes.print_all()
+
 }
